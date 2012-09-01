63b43aad8901e1e0f81ca49f0d6c27d64b54bca9
Tim Labeeuw <pair+tlabeeuw@pivotallabs.com>
2012-08-31 18:06:23 -0700

    Added a spec for the filter

--- spec/javascripts/filters/string_filter_spec.js
+++ spec/javascripts/filters/string_filter_spec.js
@@ -3,6 +3,7 @@ describe("module openAir.Filters", function () {
     describe('capitalize', function() {
       it('capitalizes the string', inject(function(capitalizeFilter) {
         expect(capitalizeFilter('foobar Baz')).toEqual('Foobar baz');
+        expect(capitalizeFilter(1)).toEqual('');
       }));
     });
   });
