babd1f5d41e5469007a58e3d892bed3b85bb11f9
Tim Labeeuw <pair+tlabeeuw@pivotallabs.com>
2012-09-01 14:23:37 -0700

    Improved upon the the date filters

--- app/assets/javascripts/filters/open_air_filters.js
+++ app/assets/javascripts/filters/open_air_filters.js
@@ -1,6 +1,10 @@
 OpenAir.module.filter('openAirDate', function() {
   return function(date) {
-    date = date.Date;
-    return new Date(date.year, date.month, date.day, date.hour, date.minute, date.second);
+    if (date && date.Date) {
+      date = date.Date;
+      return new Date(date.year, date.month, date.day, date.hour, date.minute, date.second);
+    } else {
+      return null;
+    }
   };
 });
---
+++ spec/javascripts/filters/open_air_filters_spec.js
@@ -0,0 +1,16 @@
+describe("module openAir.Filters", function () {
+  describe("OpenAir", function () {
+    describe('Date', function() {
+      it('returns a JS date object', inject(function(openAirDateFilter) {
+        var date = new Date(),
+          openAirDate = {Date: {
+            year: date.getFullYear(), month:date.getMonth(), day:date.getDate(),
+            hour: date.getHours(), minute:date.getMinutes(), second:date.getSeconds()
+          }};
+
+        expect(openAirDateFilter(openAirDate).toString()).toEqual(date.toString());
+        expect(openAirDateFilter({})).toBeNull();
+      }));
+    });
+  });
+});
